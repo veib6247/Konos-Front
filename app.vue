8d07367612fee9216aa827e810440d50e7547b5b
Bryan Olandres <ozoneblacklight@outlook.com>
2024-10-01 15:40:39 +0800

    Initial commit

---
+++ app.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="flex h-dvh w-full flex-col">
+        <!-- top -->
+        <AppTopNav />
+
+        <!-- bottom -->
+        <div class="flex h-full w-full">
+            <!-- left -->
+            <div
+                class="flex h-full w-64 flex-col border-r border-r-green-300/30 px-6 py-4"
+            >
+                <UVerticalNavigation :links="links" />
+            </div>
+
+            <!-- right -->
+            <div class="h-full w-full">
+                <NuxtPage />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+    useHead({
+        title: 'Kronos',
+    })
+
+    const links = [
+        {
+            label: 'Home',
+            icon: 'i-heroicons-home',
+            to: '/',
+        },
+    ]
+</script>
---
+++ components/AppTopNav.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="flex h-16 flex-row border-b border-b-green-300/30">
+        <!-- left -->
+        <div class="flex w-1/2 px-6">
+            <div class="my-auto flex flex-col gap-0">
+                <h1 class="text-primary text-lg font-semibold">Kronos</h1>
+                <p class="text-xs font-light">Timestamp tracking in Slack</p>
+            </div>
+        </div>
+
+        <!-- right -->
+        <div class="flex w-1/2 flex-row-reverse px-6">
+            <div class="my-auto flex flex-col">
+                <p class="text-xs">Logout</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+    useHead({
+        title: 'Kronos | Home',
+    })
+</script>
+
+<style></style>

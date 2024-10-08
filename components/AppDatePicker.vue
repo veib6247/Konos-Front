9a77582b0897a0205e1326b0ae485a3abcc6fb31
Bryan Olandres <ozoneblacklight@outlook.com>
2024-10-08 11:20:47 +0800

    moved datepicker css to component level

--- components/AppDatePicker.vue
+++ components/AppDatePicker.vue
@@ -29,4 +29,40 @@
     }
 </script>
 
-<style></style>
+<style>
+    .dp__theme_dark {
+        --dp-font-size: 13px;
+        --dp-border-radius: 0.375rem;
+        --dp-cell-border-radius: 0.375rem;
+        --dp-background-color: #111827;
+        --dp-text-color: #fff;
+        --dp-hover-color: #484848;
+        --dp-hover-text-color: #fff;
+        --dp-hover-icon-color: #959595;
+        --dp-primary-color: #4ade80;
+        --dp-primary-disabled-color: #61a8ea;
+        --dp-primary-text-color: #fff;
+        --dp-secondary-color: #a9a9a9;
+        --dp-border-color: rgb(55 65 81);
+        --dp-menu-border-color: rgb(55 65 81);
+        --dp-border-color-hover: rgb(55 65 81);
+        --dp-border-color-focus: rgb(55 65 81);
+        --dp-disabled-color: #737373;
+        --dp-disabled-color-text: #d0d0d0;
+        --dp-scroll-bar-background: #212121;
+        --dp-scroll-bar-color: #484848;
+        --dp-success-color: #00701a;
+        --dp-success-color-disabled: #428f59;
+        --dp-icon-color: #959595;
+        --dp-danger-color: #e53935;
+        --dp-marker-color: #e53935;
+        --dp-tooltip-color: #3e3e3e;
+        --dp-highlight-color: #4ade80;
+        --dp-range-between-dates-background-color: var(
+            --dp-hover-color,
+            #484848
+        );
+        --dp-range-between-dates-text-color: var(--dp-hover-text-color, #fff);
+        --dp-range-between-border-color: var(--dp-hover-color, #fff);
+    }
+</style>

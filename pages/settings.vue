f0c33c9d03929e813fa87fc90ccf7d9173b9ba5a
Bryan Olandres <ozoneblacklight@outlook.com>
2024-10-21 17:25:11 +0800

    add table for valid channels

--- pages/settings.vue
+++ pages/settings.vue
@@ -6,10 +6,149 @@
         >
             <UToggle size="xs" v-model="showRawRowData" />
         </UFormGroup>
+
+        <UFormGroup
+            size="xs"
+            label="Valid Channels"
+            class="w-1/2"
+            description="The following are channels are where the Kronos app has been added to Slack"
+        >
+            <UTable :columns="columns" :loading="isTableLoading" :rows="rows">
+                <template #actions-data="{ row }">
+                    <UButton
+                        color="gray"
+                        variant="ghost"
+                        icon="heroicons:trash"
+                        @click="deleteSelectedRow(row)"
+                    />
+                </template>
+            </UTable>
+
+            <div>
+                <UButton
+                    label="Add new channel"
+                    icon="heroicons:chat-bubble-left-right-16-solid"
+                    @click="showModal = true"
+                />
+            </div>
+        </UFormGroup>
+
+        <UModal v-model="showModal">
+            <div class="flex flex-col gap-2 p-6">
+                <h1 class="text-xl font-semibold">Add Channel</h1>
+                <div class="flex flex-col gap-2">
+                    <UFormGroup size="xs" label="ID">
+                        <UInput placeholder="C06AKRJXXXX" v-model="channelId" />
+                    </UFormGroup>
+
+                    <UFormGroup size="xs" label="Name">
+                        <UInput
+                            placeholder="xx-xxxxxxxx"
+                            v-model="channelName"
+                        />
+                    </UFormGroup>
+
+                    <div class="flex flex-row-reverse">
+                        <UButton
+                            label="Save"
+                            icon="heroicons:check"
+                            @click="saveChannel"
+                        />
+                    </div>
+                </div>
+            </div>
+        </UModal>
     </div>
 </template>
 
 <script lang="ts" setup>
+    // composables
     useUpdateTitle('Settings')
+
+    // states
     const showRawRowData = useState<boolean>('showRawRowData')
+    const supabase = useSupabaseClient()
+    const isTableLoading = ref(true)
+    const tableName = 'Valid Channels'
+    const showModal = ref(false)
+    const channelId = ref('')
+    const channelName = ref('')
+
+    const columns = [
+        {
+            key: 'channel_id',
+            label: 'ID',
+        },
+        {
+            key: 'channel_name',
+            label: 'Name',
+        },
+        {
+            key: 'actions',
+        },
+    ]
+
+    const rows = ref([])
+
+    onMounted(async () => {
+        await loadData()
+    })
+
+    async function loadData() {
+        isTableLoading.value = true
+        rows.value = []
+
+        const query = supabase.from(tableName).select('*')
+        const { data, error } = await query
+
+        if (error) {
+            console.error(error)
+        }
+
+        if (data) {
+            for (const item of data) {
+                rows.value.push(item)
+            }
+        }
+
+        isTableLoading.value = false
+    }
+
+    async function deleteSelectedRow(row: ChannelItem) {
+        const { error } = await supabase
+            .from(tableName)
+            .delete()
+            .eq('channel_id', row.channel_id)
+
+        if (error) {
+            console.error(error)
+        }
+
+        await loadData()
+    }
+
+    async function saveChannel() {
+        const query = supabase.from('Valid Channels').insert([
+            {
+                channel_id: channelId.value,
+                channel_name: channelName.value,
+            },
+        ])
+
+        const { data, error } = await query
+
+        if (error) {
+            console.error(error)
+        }
+
+        if (data) {
+            console.info(data)
+
+            channelId.value = ''
+            channelName.value = ''
+        }
+
+        showModal.value = false
+        await loadData()
+    }
 </script>

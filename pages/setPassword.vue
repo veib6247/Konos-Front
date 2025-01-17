71bb3db303478e2735094bda03110932f4d9d5cf
Bryan Olandres <ozoneblacklight@outlook.com>
2025-01-17 10:49:11 +0800

    add confirm password

--- pages/setPassword.vue
+++ pages/setPassword.vue
@@ -8,7 +8,7 @@
                 <p class="text-xs font-light">Timestamp tracking in Slack</p>
             </div>
 
-            <div class="space-y-2" v-if="token">
+            <div class="flex flex-col gap-2" v-if="token">
                 <p class="text-xs font-light">Please set your new password</p>
 
                 <UForm
@@ -21,8 +21,19 @@
                         <UInput v-model="state.password" type="password" />
                     </UFormGroup>
 
+                    <UFormGroup
+                        size="xs"
+                        label="Confirm Password"
+                        name="confirmPassword"
+                    >
+                        <UInput
+                            v-model="state.confirmPassword"
+                            type="password"
+                        />
+                    </UFormGroup>
+
                     <div class="flex justify-end">
-                        <UButton size="xs" type="submit">
+                        <UButton :loading="isLoading" size="xs" type="submit">
                             Set my password
                         </UButton>
                     </div>
@@ -62,6 +73,7 @@
     })
 
     const isDevMode = import.meta.env.DEV
+    const isLoading = ref(false)
     const hash = useRoute().hash.substring(1)
     const params = new URLSearchParams(hash)
     const errMsg = ref('')
@@ -69,12 +81,19 @@
     const toast = useToast()
     const schema = z.object({
         password: z.string().min(8, 'Must be at least 8 characters'),
+        confirmPassword: z
+            .string()
+            .refine(
+                (data) => data === state.password,
+                'Passwords do not match'
+            ),
     })
 
     type Schema = z.output<typeof schema>
 
     const state = reactive({
         password: undefined,
+        confirmPassword: undefined,
     })
 
     /**
@@ -82,6 +101,8 @@
      * @param event
      */
     async function onSubmit(event: FormSubmitEvent<Schema>) {
+        isLoading.value = true
+
         try {
             const data = await fetch('/api/updatePassword', {
                 method: 'POST',
@@ -123,5 +144,7 @@
             errMsg.value = msg
             console.error(msg, error)
         }
+
+        isLoading.value = false
     }
 </script>

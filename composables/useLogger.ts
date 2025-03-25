0fb82dcfd0a8f058707fbd0aa2b85ecd1c9d66f6
Bryan Olandres <ozoneblacklight@outlook.com>
2025-03-25 10:26:20 +0800

    removed pino-pretty

--- composables/useLogger.ts
+++ composables/useLogger.ts
@@ -7,14 +7,6 @@ import pino from 'pino'
 export default function () {
 	const logger = pino({
 		level: import.meta.env.PROD ? 'error' : 'info', // Only log errors in production,
-		transport: {
-			target: 'pino-pretty', // Use pino-pretty for pretty-printing
-			options: {
-				colorize: true, // Add colors to the logs
-				translateTime: 'SYS:yyyy-mm-dd HH:MM:ss', // Format timestamps
-				ignore: 'pid,hostname', // Hide unnecessary fields
-			},
-		},
 	})
 
 	return logger
